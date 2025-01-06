7499c47edd89e969f268e1949c7d53c9c322014b
MUKILTEO\1721807 <1721807@mukilteo.wednet.edu>
2025-01-06 13:31:55 -0800

    Hunter 1/6

--- 2D Template/Assets/Scrips/PlayerCombat.cs
+++ 2D Template/Assets/Scrips/PlayerCombat.cs
@@ -1,8 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class PlayerCombat : MonoBehaviour
 {
+   
+    public Animator animator;
+
+    public Transform attackPoint;
+    public float attackRange = 0.5f;
+    public LayerMask enemyLayers;
+      
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Attack();
+        }
+    }
+
+
+    void Attack()
+    {
+        // Play an attack animation
+        animator.SetTrigger("Attack");
+        // Detect eniemies in range of attack
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
+
+        // Damage them
+        foreach(Collider2D enemy in hitEnemies)
+        {
+            Debug.Log("We hit " + enemy.name);
+        }
+    }
 
 }
